9d1fa247a13cf81014cff59a0bfb8514066f28bd
Joseph Kania <jkania@andrew.cmu.edu>
2015-06-26 14:29:33 +0200

    adding shell script

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time ParselTongue clean.py --user 916 --name gr035 --dataPath  PWD:gr035.sb5.idi \
+    --fileCount 6 --cal J1232-0224 --inseq 1  --source MEX --bandPassCal J1222+0413
